3cbed25cc9d7d3fb4c5868dfbadee598199e2d0e
Yogeshkumar-026 <yogeshkanojia26@gmail.com>
2023-04-11 21:28:12 +0530

    Added problem solu

---
+++ stack.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#define MAX_SIZE 100
+
+int stack[MAX_SIZE];
+int top = -1;
+
+void push(int value) {
+  stack[++top] = value;
+}
+
+int pop() {
+  return stack[top--];
+}
+
+int peek() {
+  return stack[top];
+}
+
+
+void find_greatest_to_left(int arr[], int n) {
+  int i;
+  printf("Greatest elements to the left of each element:\n");
+  for (i = 0; i < n; i++) {
+    while (top != -1 && peek() <= arr[i]) {
+      pop();
+    }
+    if (top == -1) {
+      printf("-1 ");
+    } else {
+      printf("%d ", peek());
+    }
+    push(arr[i]);
+  }
+}
+
+
+void find_greatest_to_right(int arr[], int n) {
+  int i;
+  printf("Greatest elements to the right of each element:\n");
+  for (i = n - 1; i >= 0; i--) {
+    while (top != -1 && peek() <= arr[i]) {
+      pop();
+    }
+    if (top == -1) {
+      printf("-1 ");
+    } else {
+      printf("%d ", peek());
+    }
+    push(arr[i]);
+  }
+}
+
+
+
+void find_smallest_to_right(int arr[], int n) {
+  int i;
+  printf("Smallest elements to the right of each element:\n");
+  for (i = n - 1; i >= 0; i--) {
+    while (top != -1 && peek() >= arr[i]) {
+      pop();
+    }
+    if (top == -1) {
+      printf("-1 ");
+    } else {
+      printf("%d ", peek());
+    }
+    push(arr[i]);
+  }
+}
+
+
+void find_smallest_to_left(int arr[], int n) {
+  int i;
+  printf("Smallest elements to the left of each element:\n");
+  for (i = 0; i < n; i++) {
+    while (top != -1 && peek() >= arr[i]) {
+      pop();
+    }
+    if (top == -1) {
+      printf("-1 ");
+    } else {
+      printf("%d ", peek());
+    }
+    push(arr[i]);
+  }
+}
+
